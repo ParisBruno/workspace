281422a2e2f455bb9fa6b186d95101cf79a66212
Bruno <brunofiuggi@gmail.com>
2017-10-12 12:17:28 +0000

    Uploading latest changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009113925) do
+ActiveRecord::Schema.define(version: 20171012110940) do
 
   create_table "chefs", force: :cascade do |t|
     t.datetime "updated_at"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171009113925) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
 end
