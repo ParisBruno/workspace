ae607f1aa393aff4848ded3890e2f7088fcba22b
Bruno <brunofiuggi@gmail.com>
2017-10-13 17:10:53 +0000

    add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012110940) do
+ActiveRecord::Schema.define(version: 20171013151159) do
 
   create_table "chefs", force: :cascade do |t|
     t.datetime "updated_at"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171012110940) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
